ad1b7bc7844bb66864ec0333f31859c279cf5993
root <admin@autodidactati.com>
2022-02-26 20:59:02 -0300

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Descripcion en Reporte de la Factura Electrónica Argentina",
+    'version': '14.0',
+    'author': "Autodidacta TI, Moldeo Interactive, Exemax, Codize, ADHOC SA, Odoo Community Association (OCA)",
+    'category': 'Accounting/Localizations',
+    'depends': [
+        'l10n_ar_afipws_fe'
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+    'data': [
+        'report/report_invoice_fe_inherit.xml',
+    ],
+    'demo': []
+}
